52783e5d77dccdf89eda22fe2ed9fb32082be7da
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-05-06 13:34:37 +0200

    Pridaná migrácia pre MySQL

--- Data/Migrations/20200506113233_MySQL.Designer.cs
+++ Data/Migrations/20200506113233_MySQL.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizWebApp.Data;
 
 namespace QuizWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200506113233_MySQL")]
+    partial class MySQL
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
