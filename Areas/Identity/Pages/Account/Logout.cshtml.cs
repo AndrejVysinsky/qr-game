3c7cd63baa17812a8df01b0613d87eacd02951ff
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-06-12 13:50:37 +0200

    Pridané -> ak sa používateľ odhlási a nemá nastavené heslo, účet sa vymaže

---
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using QuizWebApp.Models;
+
+namespace QuizWebApp.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            var user = await _signInManager.UserManager.FindByNameAsync(User.Identity.Name);
+
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+
+            if (user.PasswordHash == null)
+                await _signInManager.UserManager.DeleteAsync(user);
+
+
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+    }
+}
